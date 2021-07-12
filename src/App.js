fa5250fcd7b10eed4e22a61797c108248ede63f1
wonlee6 <wonlee6@gmail.com>
2021-07-12 17:03:32 +0900

    login UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Login from './components/login/login';
+import styles from './App.module.css';
 
 const App = ({ authService }) => {
-  return <Login authService={authService} />;
+  return (
+    <div className={styles.app}>
+      <Login authService={authService} />;
+    </div>
+  );
 };
 
 export default App;
