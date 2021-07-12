c9d2879b1745e475ed05dc75db37a6fd95b02967
wonlee6 <wonlee6@gmail.com>
2021-07-12 17:51:02 +0900

    router로 화면 전환

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Login from './components/login/login';
 import styles from './App.module.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Maker from './components/maker/maker';
 
+// exact = 주어진 경로와 정확히 맞아 떨어져야만 설정한 컴포넌트를 보여준다.
 const App = ({ authService }) => {
   return (
     <div className={styles.app}>
-      <Login authService={authService} />;
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Login authService={authService} />;
+          </Route>
+          <Route exact path='/maker'>
+            <Maker authService={authService} />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './login.module.css';
 
+// uid(유니크한 값) 통해 처리
 const Login = ({ authService }) => {
+  const history = useHistory();
+  const goToMaker = (userId) => {
+    history.push({
+      pathname: '/maker',
+      state: { id: userId },
+    });
+  };
   const onLogin = (event) => {
     authService //
       .login(event.currentTarget.textContent)
-      .then(console.log);
+      .then((data) => goToMaker(data.user.uid));
   };
+
+  // 사용자의 정보를 가지고 있을 경우, 로그인 페이지가 아닌 페이지로 이동
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      user && goToMaker(user.id);
+    });
+  });
   return (
     <section className={styles.login}>
       <Header />
