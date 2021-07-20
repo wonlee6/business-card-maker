1d4d9c3ff4d0362c9cd71ee190c3844cdbdd60b9
wonlee6 <wonlee6@gmail.com>
2021-07-20 23:54:44 +0900

    google map

--- src/components/googleMap/googleMap.jsx
+++ src/components/googleMap/googleMap.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import styles from './googleMap.module.css';
+import GoogleMapReact from 'google-map-react';
 
 const GoogleMap = ({ coronaItem }) => {
   return (
-    <section>
-      <h1>google</h1>
+    <section className={styles.googleMap}>
+      <h1 className={styles.title}>지도</h1>
+      <div className={styles.container}>
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: 'AIzaSyD5M-XjaBrcCsmwOikc2NkCHI-TnmyIt1g',
+          }}
+          defaultCenter={{ lat: 37.5, lng: 127 }}
+          defaultZoom={15}
+        >
+          {/* <AnyReactComponent lat={59.955413} lng={30.337844} text='My Marker' /> */}
+        </GoogleMapReact>
+      </div>
     </section>
   );
 };
